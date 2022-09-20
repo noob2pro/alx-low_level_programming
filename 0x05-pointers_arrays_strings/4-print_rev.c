d77ae739cb4cbca86368f2567bd7f3ec322e7657
noob2pro <mail4chaz@yahoo.com>
2022-09-20 02:57:52 -0700

    commit 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,30 @@
 
 void print_rev(char *s)
 {
-	int len = 0, i;
+/*
+*	int len = 0, i;
+*
+*	while (s[i++])
+*		len++;
+*
+*	for (i = len - 1; i >= 0; i--)
+*		_putchar(s[i]);
+*
+*	_putchar('\n');
+*/
 
-	while (s[i++])
-		len++;
+	char *r_ptr = s;
+	int i;
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
+	while (*(r_ptr + 1) != '\0')
+		r_ptr++;
 
-	_putchar('\n');
+	while (r_ptr > s)
+	{
+		char tmp = *r_ptr;
+		*r_ptr-- = *s;
+		*s++ = tmp;
+		 _putchar(s[i]);
+	}
+
 }

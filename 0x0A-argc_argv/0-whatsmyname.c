e9dda5cbae3a8876b5ccaec64eb2b93c79f7c759
noob2pro <mail4chaz@yahoo.com>
2022-10-02 02:09:34 -0700

    commit 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  * Return: 0
  */
 
-int function main(int argc, char *argv[])
+int function main(argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	(void)argc;
+
 	return (0);
 }

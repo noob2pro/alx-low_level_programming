dac8d8592b79bc765d99124b5e359987268af01a
noob2pro <mail4chaz@yahoo.com>
2022-10-03 02:13:18 -0700

    commit 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments received
+ * @argv: arguments passed
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: arguments received
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	
+}

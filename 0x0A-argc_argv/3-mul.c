65180e5eedeee1e7eaaa7f3c37bf7f21d6a2fa51
noob2pro <mail4chaz@yahoo.com>
2022-10-03 05:30:58 -0700

    commit 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,5 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-	
+	if (argc < 3)
+	{
+		printf("Error\n");
+	}
+
+	int num1, num2;
+
+	num1 = argv[1];
+	num2 = argvc[2];
+
+	printf("%d\n", num1 * num2);
+
+	return (0);
 }

a4aa1ebf12a8c4792c28127fbb39cf6d1cf093d0
noob2pro <mail4chaz@yahoo.com>
2022-10-03 07:54:59 -0700

    declared variables at the top

--- 0x0A-argc_argv/3.1-mul.c
+++ 0x0A-argc_argv/3.1-mul.c
@@ -12,13 +12,13 @@
 
 int main(int argc, char *argv[])
 {
+	int num1, num2;
+
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int num1, num2;
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);

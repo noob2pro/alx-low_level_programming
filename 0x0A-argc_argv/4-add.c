b6c5cd515ecef67dfb732ccda8d09906fa7b91f3
noob2pro <mail4chaz@yahoo.com>
2022-10-04 02:30:18 -0700

    commit 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main -> adds positive numbers
+ * @argc: number of arguments passed
+ * @argv: arguments received
+ *
+ * Return: 0 on success 1 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int i, sum;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (i = 0; i < argc; i++;)
+	{
+		if (isdigit(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum = sum + argv[i];
+		}
+	}
+
+	return (0);
+}

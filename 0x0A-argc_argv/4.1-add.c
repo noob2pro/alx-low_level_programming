7a0ab61d98af4a89484bed18406594f8996f64d8
noob2pro <mail4chaz@yahoo.com>
2022-10-05 18:41:26 -0700

    commit 0-create_array.c

--- 0x0A-argc_argv/4.1-add.c
+++ 0x0A-argc_argv/4.1-add.c
@@ -14,7 +14,7 @@
 int main(int argc, char *argv[])
 {
 
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 
@@ -24,13 +24,16 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	for (i = 1; i < argc; i++)
+	for (i = 1; i < argc - 1; i++)
 	{
-
-		if (!atoi(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+
+			if (isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
 		sum = sum + atoi(argv[i]);
